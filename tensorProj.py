1e360c6e0f28160f1d9ac778c8006294d114228e
landesko <dale2@umbc.edu>
2016-11-28 00:59:47 -0800

    Created a hello world program using tensor flow

---
+++ tensorProj.py
@@ -0,0 +1,13 @@
+# Importing some common modules
+import os, sys
+import pprint
+import time
+import tensorflow as tf
+
+if __name__ == '__main__':
+	hello = tf.constant('Hello, TensorFlow!')
+	sess = tf.Session()
+	print(sess.run(hello))
+
+
+
